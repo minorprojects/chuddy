425e9757469de20f69ca5073ef01bc47fcb4cf5f
Emmanuel Chima <71467633+chiho-5@users.noreply.github.com>
2023-09-01 10:45:38 +0100

    Create train.py

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+from datasets import load_dataset
+import transformers
+from tqdm import tqdm
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from Models.chuddy import Chuddy
+
+
+  
+  
+  
