25c12a906c2c112db9f0b93616bfb2bc9f2bdeb9
Emmanuel Chima <71467633+chiho-5@users.noreply.github.com>
2023-09-01 10:09:39 +0100

    Update configuration.py

--- Models/configuration.py
+++ Models/configuration.py
@@ -1,1 +1,15 @@
+# Model configuration class for the Visual encoder, qformer and Language model
 
+import torch
+
+class ModelConfig(nn.Module):
+  def __init__(self,
+               bert_config = 'bert-base-uncased',
+               llama_config = '',
+               beit_config = 'microsoft/beit-base-patch16-224-pt22k'
+              ):
+                super().__init__()
+                self.bert_config = bert_config
+                self.llama_config = llama_config
+                self.beit_config = beit_config
+                
