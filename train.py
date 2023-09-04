24d89ea2c624a7e0cdb190e78ab8340e4b855415
Emmanuel Chima <71467633+chiho-5@users.noreply.github.com>
2023-09-04 15:36:41 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.Functional as F
 from torch import nn
 from datasets import load_dataset
 import transformers
@@ -7,6 +8,35 @@ import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from Models.chuddy import Chuddy
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+lr = 0.001
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+model = Chuddy()
+model = model.to(device)
+save_path= 
+
+
+
+def train(model,devivce,data_loader,epochs):
+  for epoch in range(epochs):
+  
+    model.train()
+    total_loss = 0.0
+    for i,(image,text) in enumerate(data_loader):
+      image = image.to(device)
+      text = text.to(device)
+      optimizer.zero_grad()
+      loss_itc,loss_itm,loss_lm,loss_hyp = model(image,text)
+      loss = loss_itc + loss_itm + loss_lm
+      loss.backward()
+      optimizer.step()
+      total_loss = loss.item()
+    print('epoch {} step {} loss: {}'.format(epoch,i + 1, total_loss))
+return total_loss
+
+torch.save(model_state_dict(),save_path)
+    
+    
 
   
   
