e5c8b64be31edd6e0909e4a9dee3fa2677db9d15
Emmanuel Chima <71467633+chiho-5@users.noreply.github.com>
2023-09-04 16:04:05 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
-LEARNING_RATE = 
-EPOCHS = 
-DEVICE = 
-NUM_WORKERS = 
-DATA_LINK=
-SAVE_PATH =
+LEARNING_RATE = 0.001
+EPOCHS = 10
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+NUM_WORKERS = 4
+DATA_LINK= ''
+SAVE_PATH = ''
