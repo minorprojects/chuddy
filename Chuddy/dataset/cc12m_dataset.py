aeb18c3b66996d5eb9ff990d868bf0115431f0a3
Emmanuel Chima <71467633+chiho-5@users.noreply.github.com>
2023-09-30 09:25:21 +0100

    Update cc12m_dataset.py

--- Chuddy/dataset/cc12m_dataset.py
+++ Chuddy/dataset/cc12m_dataset.py
@@ -35,7 +35,46 @@ def fetch_images(batch, num_threads, timeout=None, retries=0):
     return batch
 
 
+
+
+class ImageTextDataset(torch.utils.data.Dataset):
+    def __init__(self, hf_dataset, *, encoder_name: str, max_length: int,
+                 side_length: int, train: bool = True, img_transform=None):
+        split = "train" if train else "validation"
+
+        self.urls = hf_dataset[f"{split}"]['image_url']
+        self.captions = hf_dataset[f"{split}"]['caption']
+
+        if img_transform is None:
+            self.img_transform = Compose([ToTensor(), _Rescale(side_length)])
+        else:
+            self.img_transform = Compose([ToTensor(), _Rescale(side_length), img_transform])
+        self.encoder_name = encoder_name
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.urls)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img = _fetch_single_image(self.urls[idx])
+        if img is None:
+            return None
+        elif self.img_transform:
+            img = self.img_transform(img)
+
+        # Have to check None again because `Resize` transform can return None
+        if img is None:
+            return None
+        elif img.shape[0] != 3:
+            return None
+
+        enc, msk = t5_encode_text([self.captions[idx]], self.encoder_name, self.max_length)
+
+        return {'image': img, 'encoding': enc, 'mask': msk}
+
 num_threads = 20
 dset = load_dataset("conceptual_12m")
 dset = dset.map(fetch_images, batched=True, batch_size=100, fn_kwargs={"num_threads": num_threads})
-
